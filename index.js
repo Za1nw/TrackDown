fa4f828e429e3242a4203eabd9d349418d4531eb
Za1nw <140191517+Za1nw@users.noreply.github.com>
2023-07-21 19:46:23 +0300

    index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="5815321594:AAHq3Zg9PZvIKZ8gyq2DqeUEjf-iXq3wI40";
 //TOGGLE for Shorters
 var use1pt=false;
 
